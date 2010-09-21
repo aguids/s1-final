df2249a5d965cbeb3cb3ed4597374b58e69ef18b
Felipe Doria <felipe.doria@gmail.com>
2010-09-21 17:15:45 -0300

    Basic row manipulation

--- lib/table.rb
+++ lib/table.rb
@@ -1,12 +1,17 @@
 # encoding: utf-8
+require "forwardable"
 
 class Table
+  extend Forwardable
   
   def initialize(array_of_rows = [])
     @table = array_of_rows
   end
   
-  def rows_count
-    @table.count
+  def rows
+    @table
   end
+  
+  def_delegators :@table, :[], :<<
+  def_delegator :@table, :count, :rows_count
 end
--- test/table/table_test.rb
+++ test/table/table_test.rb
@@ -7,19 +7,57 @@ class TableTest < Test::Unit::TestCase
   def array_of_rows
     [ ["name", "age", "job"],
       ["john", 20, nil],
-      ["steve", 40, "programmer"]
+      ["steve", 40, "programmer"],
+      ["mary", 28, "model"]
     ]
   end
   
   describe "creation" do
-    test "may start empty" do
+    test "starts empty" do
       table = Table.new
       assert_equal 0, table.rows_count
     end
     
     test "accepts an array of rows" do
       table = Table.new(array_of_rows)
-      assert_equal 3, table.rows_count
+      assert_equal array_of_rows.count, table.rows_count
+    end
+  end
+  
+  describe "row manipulation" do
+    setup do
+      @table = Table.new array_of_rows
+    end
+    
+    test "retrieve row by index" do
+      assert_equal array_of_rows[2], @table.rows[2]
+      assert_equal array_of_rows[2], @table[2]
+    end
+  end
+  
+  describe "row addition" do
+    describe "with an empty table" do
+      setup do
+        @table = Table.new
+        @row = ["stuart", 50, nil]
+        @table << @row
+      end
+      
+      test "appends the row to the start of the table" do
+        assert_equal @row, @table[0]
+      end
+    end
+    
+    describe "with an existing table" do
+      setup do
+        @table = Table.new array_of_rows
+        @row = ["stuart", 50, nil]
+        @table << @row
+      end
+      
+      test "appends the row to the end of the table" do
+        assert_equal @row, @table[@table.rows_count - 1]
+      end
     end
   end
   
