9378d07a17335c0bb32b34090f70de8429dbc7b8
Felipe Doria <felipe.doria@gmail.com>
2010-09-22 04:24:12 -0300

    Simple row and column operations

--- lib/table.rb
+++ lib/table.rb
@@ -12,6 +12,38 @@ class Table
     @table
   end
   
+  def columns
+    @table.transpose
+  end
+  
+  def columns_count
+    rows.first.count
+  end
+  
+  def append_column(column)
+    new_columns = columns << column
+    update_columns(new_columns)
+  end
+  
+  def insert_column_at(index, column)
+    new_columns = columns.insert index, column
+    update_columns(new_columns)
+  end
+  
+  def delete_column_at(index)
+    new_columns = columns
+    new_columns.delete_at index
+    update_columns(new_columns)
+  end
+  
   def_delegators :@table, :[], :<<
-  def_delegator :@table, :count, :rows_count
+  
+  def_delegator :@table, :count,     :rows_count
+  def_delegator :@table, :insert,    :insert_row_at
+  def_delegator :@table, :delete_at, :delete_row_at
+
+private
+  def update_columns(new_columns)
+    @table = new_columns.transpose
+  end
 end
--- test/table/table_test.rb
+++ test/table/table_test.rb
@@ -12,6 +12,10 @@ class TableTest < Test::Unit::TestCase
     ]
   end
   
+  def array_of_columns
+    array_of_rows.transpose
+  end
+  
   describe "creation" do
     test "starts empty" do
       table = Table.new
@@ -33,31 +37,51 @@ class TableTest < Test::Unit::TestCase
       assert_equal array_of_rows[2], @table.rows[2]
       assert_equal array_of_rows[2], @table[2]
     end
-  end
-  
-  describe "row addition" do
-    describe "with an empty table" do
-      setup do
-        @table = Table.new
-        @row = ["stuart", 50, nil]
-        @table << @row
-      end
       
-      test "appends the row to the start of the table" do
-        assert_equal @row, @table[0]
-      end
+    test "append row to the end of the table" do
+      row = ["stuart", 35, nil]
+      @table << row
+      assert_equal row, @table[@table.rows_count - 1]
     end
     
-    describe "with an existing table" do
-      setup do
-        @table = Table.new array_of_rows
-        @row = ["stuart", 50, nil]
-        @table << @row
-      end
-      
-      test "appends the row to the end of the table" do
-        assert_equal @row, @table[@table.rows_count - 1]
-      end
+    test "insert row at position" do
+      row = ["stuart", 35, nil]
+      @table.insert_row_at 2, row
+      assert_equal row, @table[2]
+    end
+    
+    test "delete row" do
+      @table.delete_row_at(2)
+      assert_not_equal array_of_rows[2], @table[2]
+      assert_not_equal array_of_rows.count, @table.rows_count
+    end
+  end
+  
+  describe "column manipulation" do
+    setup do
+      @table = Table.new array_of_rows
+    end
+    
+    test "retrieve column by index" do
+      assert_equal array_of_columns[1], @table.columns[1]
+    end
+    
+    test "append column to the right of the table" do
+      column = ["sport", "golf", nil, "soccer"]
+      @table.append_column column
+      assert_equal column, @table.columns[@table.columns_count - 1]
+    end
+
+    test "insert column at position" do
+      column = ["sport", "golf", nil, "soccer"]
+      @table.insert_column_at 1, column
+      assert_equal column, @table.columns[1]
+    end
+
+    test "delete column at position" do
+      @table.delete_column_at 1
+      assert_not_equal array_of_columns[1], @table.columns[1]
+      assert_not_equal array_of_columns.count, @table.columns_count
     end
   end
   
